9585fb836055c8b641e9a42c2ab65fe9c6a90cc6
Pascal Burlet <pascal.burlet@hsr.ch>
2021-03-16 19:16:17 +0100

    added empty codesign identity

--- .github/workflows/RunCompleteUnitTestSuite.sh
+++ .github/workflows/RunCompleteUnitTestSuite.sh
@@ -6,4 +6,4 @@
 #  Created by Pascal Burlet on 16.03.21.
 #  
 
-arch -arm64 /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild clean test -project PolynomialRegression.xcodeproj -scheme PolynomialRegression -destination "${destination}" | /usr/local/bin/xcpretty -r junit
+arch -arm64 /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild clean test -project PolynomialRegression.xcodeproj -scheme PolynomialRegression -destination "${destination}" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO ONLY_ACTIVE_ARCH=NO | /usr/local/bin/xcpretty -r junit
