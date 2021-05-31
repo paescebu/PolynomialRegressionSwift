af609954332f81113d1ff6e4583c3fc86dba9f63
Pascal Burlet <pascal.burlet@hsr.ch>
2021-05-31 20:07:30 +0200

    making sure build fail also fails CI/CD

--- .github/workflows/RunCompleteUnitTestSuite.sh
+++ .github/workflows/RunCompleteUnitTestSuite.sh
@@ -6,4 +6,7 @@
 #  Created by Pascal Burlet on 16.03.21.
 #  
 
+set -ex
+set -o pipefail
+
 arch -arm64 /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild clean test -project PolynomialRegression.xcodeproj -scheme PolynomialRegression -destination "${destination}" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO ONLY_ACTIVE_ARCH=NO | xcpretty -r junit
