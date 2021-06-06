69c668922c0ea9f9d0f0c05eed4da27b59e794dd
Pascal Burlet <pburlet@outlook.com>
2021-06-06 19:21:16 +0200

    fix CI/CD

--- .github/workflows/RunCompleteUnitTestSuite.sh
+++ .github/workflows/RunCompleteUnitTestSuite.sh
@@ -9,4 +9,13 @@
 set -ex
 set -o pipefail
 
+#try to build Unit Test Target and fail on failure
+cd implementation
+echo "Just trying to build"
+arch -arm64 /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -project PolynomialRegression.xcodeproj -scheme PolynomialRegression -destination "${destination}" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO ONLY_ACTIVE_ARCH=NO | xcpretty
+
+#Run Unit Test but dont fail on failure so that results can be parsed for github
+set +ex
+set +o pipefail
+echo "Running Unit Tests"
 arch -arm64 /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild clean test -project PolynomialRegression.xcodeproj -scheme PolynomialRegression -destination "${destination}" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO ONLY_ACTIVE_ARCH=NO | xcpretty -r junit
