706e057a1515cf6838dd952e3408240ccc3cadf2
pascalburlet <pburlet@outlook.com>
2023-09-21 21:20:05 +0200

    fix CI/CD

--- .github/workflows/RunCompleteUnitTestSuite.sh
+++ .github/workflows/RunCompleteUnitTestSuite.sh
@@ -11,11 +11,11 @@ set -o pipefail
 
 #try to build Unit Test Target and fail on failure
 echo "Just trying to build"
-arch -arm64 swift build
+swift build
 
 #Run Unit Test but dont fail on failure so that results can be parsed for github
 set +ex
 set +o pipefail
 echo "Running Unit Tests"
 
-arch -arm64 /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -scheme PolynomialRegressionSwift -destination "platform=iOS Simulator,name=iPhone 12" -enableCodeCoverage YES -derivedDataPath "derivedData" clean test | xcpretty -r junit
+xcodebuild -scheme PolynomialRegressionSwift -destination "platform=iOS Simulator,name=iPhone 12" -enableCodeCoverage YES -derivedDataPath "derivedData" clean test | xcpretty -r junit
